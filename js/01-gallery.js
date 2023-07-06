63472dcb1a197bdf179b9c644c914e98e484166f
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-07-06 14:23:29 +0300

    hw07js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,86 +1,49 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onImgClick);
 
-// const instance = basicLightbox.create(`
-//     <h1>Dynamic Content</h1>
-//     <p>You can set the content of the lightbox with JS.</p>
-// `)
-// export const galleryItems = [
-//     {
-//       preview:
-//         'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
-//       original:
-//         'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-//       description: 'Hokkaido Flower',
-//     },
-// const markup = galleryItems.map(({preview, original, description}) => 
-// `<li><img src="${preview}" alt="${original}"><h2>${description}</h2></li>`);
-
-
-// galleryItems.insertAdjacentHTML('beforeend', markup.join(''));
-
-
-const ulRef = document.querySelector('.js-gallery');
-const modalRef = document.querySelector('.js-lightbox');
-const backdropRef = document.querySelector('.lightbox__overlay');
-const imageRef = document.querySelector('.lightbox__image');
-
-
-const closeBtn = document.querySelector('[data-action="close-lightbox"]');
-
-
-const cartMarkup = createPictureCartMarkup(galleryItems);
-ulRef.insertAdjacentHTML('beforeend', cartMarkup);
-
-ulRef.addEventListener('click', onPictureClick);
-closeBtn.addEventListener('click', onCloseBtnClick)
-modalRef.addEventListener('click', onBackdropClick);
- 
-
-
-function createPictureCartMarkup(galleryItems) {
-  return galleryItems
+// rendered items
+function createGalleryItemsMarkup(items) {
+  return items
     .map(({ preview, original, description }) => {
-      return `
-    <li class="gallery__item">
-      <a
-        class="gallery__link"
-        href = '${original}';
-      >
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-    </li>
-    `;
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
     })
     .join('');
-};
-
-function onPictureClick(event) {
-  event.preventDefault();
-  if (!event.target.classList.contains('gallery__image')) {
-    return;
+}
+const instance = basicLightbox.create(
+  `
+<img width="1280" height="auto" src="">`,
+  {
+    onShow: (instance) => {
+      window.addEventListener('keydown', onEscKeyPress);
+    },
+    onClose: (instance) => {
+      window.removeEventListener('keydown', onEscKeyPress);
+    },
   }
-  // console.log(event.target);
-  window.addEventListener('keydown', onEscBtnPress);
-  modalRef.classList.add('is-open');
-  imageRef.src = event.target.dataset.source;
+);
+
+function onImgClick(e) {
+  e.preventDefault();
+  const datasetSource = e.target.dataset.source;
+  if (!datasetSource) return;
+  instance.element().querySelector('img').src = datasetSource;
+  instance.show();
 }
 
-function onCloseBtnClick() {
-  modalRef.classList.remove('is-open');
-  window.removeEventListener('keydown', onEscBtnPress);
-  imageRef.src = '';
-}
-
-function onBackdropClick(event) {
-  if (backdropRef === event.target) {
-    onCloseBtnClick();
-  }
+function onEscKeyPress(e) {
+  if (e.code !== 'Escape') return;
+  instance.close();
 }
