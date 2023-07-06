6db56691ce51ca961bbe65140ac30f517ac59e3a
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-07-06 14:34:25 +0300

    hw07js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,25 +1,25 @@
 import { galleryItems } from './gallery-items.js';
+
+const galleryContainer = document.querySelector('.gallery');
 // Change code below this line
 
-console.log(galleryItems);
-const galleryContainer = document.querySelector('.gallery');
 const markup = galleryItems.reduce(
-    (acc, { original, preview, description }) =>
-      (acc += `<li>
-    <a class="gallery__item" href="${original}">
-      <img
-        class="gallery__image"
-        src="${preview}"
-        alt="${description}"
-      />
-    </a>
-  </li>`),
-    ''
-  );
-  
-  galleryContainer.insertAdjacentHTML('beforeend', markup);
-  
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-  });
+  (acc, { original, preview, description }) =>
+    (acc += `<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`),
+  ''
+);
+
+galleryContainer.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
