247ada23554e26fafc291bbbd2f37224f1e928a1
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-07-06 13:41:11 +0300

    hw06js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -22,25 +22,71 @@ console.log(galleryItems);
 // galleryItems.insertAdjacentHTML('beforeend', markup.join(''));
 
 
-const galleryContainer = document.querySelector('.gallery');
-const itemsMarkup = createGalleryItemsMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
-galleryContainer.addEventListener('click', onImgClick);
+const ulRef = document.querySelector('.js-gallery');
+const modalRef = document.querySelector('.js-lightbox');
+const backdropRef = document.querySelector('.lightbox__overlay');
+const imageRef = document.querySelector('.lightbox__image');
 
-// items
-function createGalleryItemsMarkup(items) {
-  return items
+
+const closeBtn = document.querySelector('[data-action="close-lightbox"]');
+
+
+const cartMarkup = createPictureCartMarkup(galleryItems);
+ulRef.insertAdjacentHTML('beforeend', cartMarkup);
+
+ulRef.addEventListener('click', onPictureClick);
+closeBtn.addEventListener('click', onCloseBtnClick)
+modalRef.addEventListener('click', onBackdropClick);
+ 
+
+
+function createPictureCartMarkup(galleryItems) {
+  return galleryItems
     .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`;
+      return `
+    <li class="gallery__item">
+      <a
+        class="gallery__link"
+        href = '${original}';
+      >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+    `;
     })
     .join('');
+};
+
+function onPictureClick(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  // console.log(event.target);
+  window.addEventListener('keydown', onEscBtnPress);
+  modalRef.classList.add('is-open');
+  imageRef.src = event.target.dataset.source;
+}
+
+function onCloseBtnClick() {
+  modalRef.classList.remove('is-open');
+  window.removeEventListener('keydown', onEscBtnPress);
+  imageRef.src = '';
+}
+
+function onBackdropClick(event) {
+  if (backdropRef === event.target) {
+    onCloseBtnClick();
+  }
+}
+function onEscBtnPress(event) {
+  console.log(event);
+  if (event.code === 'Escape') {
+    onCloseBtnClick();
+  }
 }
